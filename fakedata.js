b05aa631e41fac274fca5e3f23fc4b33133bbcaa
unknown <mayedakonika@gmail.com>
2022-12-19 14:43:06 +0600

    get api for home or default added

---
+++ fakedata.js
@@ -0,0 +1,47 @@
+[
+ {
+    "service_id":"01",
+    "title":"Bird in the brance of tree",
+    "img":"https://i.ibb.co/mcjSz6N/service-1.png",
+    "price":"30.00",
+    "description":"This whole picture brought us to closer with nature.",
+    "Owner":"Andrew robert",
+    "rating":"4.5"
+ },
+ {
+    "service_id":"01",
+    "title":"Bird in the brance of tree",
+    "img":"https://i.ibb.co/mcjSz6N/service-1.png",
+    "price":"30.00",
+    "description":"This whole picture brought us to closer with nature.",
+    "Owner":"Andrew robert",
+    "rating":"4.5"
+ },
+ {
+    "service_id":"01",
+    "title":"Bird in the brance of tree",
+    "img":"https://i.ibb.co/mcjSz6N/service-1.png",
+    "price":"30.00",
+    "description":"This whole picture brought us to closer with nature.",
+    "Owner":"Andrew robert",
+    "rating":"4.5"
+ },
+ {
+    "service_id":"01",
+    "title":"Bird in the brance of tree",
+    "img":"https://i.ibb.co/mcjSz6N/service-1.png",
+    "price":"30.00",
+    "description":"This whole picture brought us to closer with nature.",
+    "Owner":"Andrew robert",
+    "rating":"4.5"
+ },
+ {
+    "service_id":"01",
+    "title":"Bird in the brance of tree",
+    "img":"https://i.ibb.co/mcjSz6N/service-1.png",
+    "price":"30.00",
+    "description":"This whole picture brought us to closer with nature.",
+    "Owner":"Andrew robert",
+    "rating":"4.5"
+ }
+]
--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
 const cors = require("cors");
 const app = express();
 require("dotenv").config();
@@ -6,6 +8,28 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.BD_USER}:${process.env.DB_PASSWORD}@genius-car-cluster.etmukv1.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run(){
+    try{
+      const reviewsCollection=client.db('serviceReviewer').collection('services')
+      app.get("/", async (req, res) => {
+        const query = {};
+        const cursor = reviewsCollection.find(query);
+        const services = await cursor.limit(3).toArray();
+        res.send(services);
+      });
+    }
+    finally{
+
+    }
+
+}
+run().catch(error=>console.log(error))
+
 
 app.get("/", (req, res) => {
   res.send("reviewing api is coming");
