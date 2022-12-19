27b431667fac639b4abae613780dc0db8f5dca59
unknown <mayedakonika@gmail.com>
2022-12-19 17:13:58 +0600

    services api added

--- index.js
+++ index.js
@@ -22,6 +22,13 @@ async function run(){
         const services = await cursor.limit(3).toArray();
         res.send(services);
       });
+      
+      app.get("/services", async (req, res) => {
+        const query = {};
+        const cursor = reviewsCollection.find(query);
+        const services = await cursor.toArray();
+        res.send(services);
+      });
     }
     finally{
 
